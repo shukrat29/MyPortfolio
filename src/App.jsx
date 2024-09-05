7c14c784975fa0b2323fefa03f790435030370b1
Shukra Tamang <shukratamang.npl@gmail.com>
2024-09-05 23:25:14 +1000

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <h1>Welcome to my Personal Portfolio</h1>
-    </>
+    <div>
+      <Navbar />
+    </div>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img
+        className="logo"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8yD6XV0dDiDaHg5PSk15zR818E_dAIDpfdg&s"
+        alt=""
+      />
+
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>My Work</li>
+        <li>Contact</li>
+      </ul>
+      <div className="nav-connect">Connect with Me</div>
+    </div>
+  );
+};
+
+export default Navbar;
